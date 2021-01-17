85403ac60594a4330e2676d10e3aebbe19db41f2
CapacitorSet <capacitorset@gmail.com>
2021-01-17 18:53:37 +0100

    Move to jwlss.pw

--- temperature/source/esporta.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM dati WHERE valore = -999; # Dati non validi
-
-# Cambiare il nome del file se necessario su Windows
-SELECT * FROM view_dati INTO OUTFILE '/tmp/medie.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
--- temperature/source/estrai.sh
+++
@@ -1,7 +0,0 @@
-unzip '*.zip'
-cat *.csv > dati.csv
-
-# Cambiare i dati di accesso, ed eventualmente --host, come necessario
-mysqlimport --local --columns centralina,data,valore --fields-terminated-by=, --user root -p matematica `pwd`/dati.csv
-
-rm *.csv
